a2a1a1b5f09d5bf3caa5e9b740b7287ead9ce578
Mazi7799 <maziarghareni7799@yahoo.com>
2026-09-23 23:24:15 +0330

    fix: resolve #2

--- simple_math.py
+++ simple_math.py
@@ -1,7 +1,16 @@
 def multiplication(x: float, y: float) -> float:
     return x * y
 
+print('multiplication')
 x = float(input('Enter First Digit:'))
 y = float(input('Enter Second Digit:'))
-
 print(multiplication(x, y))
+
+
+def power(x: float, y: float) -> float:
+    return x ** y
+
+print('power')
+x = float(input('Enter First Digit:'))
+y = float(input('Enter Second Digit:'))
+print(power(x, y))
